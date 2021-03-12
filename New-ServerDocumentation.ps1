ff8722c442f3149e10812c17e41e8fab0a22456d
Luke Leigh <luke.leigh@gmail.com>
2021-03-12 23:49:49 +0000

    Add SYDI to project

---
+++ New-ServerDocumentation.ps1
@@ -0,0 +1,8 @@
+function New-ServerDocumentation {
+    param (
+        OptionalParameters
+    )
+    
+    cscript.exe D:\LazyWinAdmin\scripts\Sydi-Server.vbs -wabefghipPqrsSu -racdklp -ew -f10 -d -oD:\ServerDocumentation\ACS02.docx -tACS02 -u'lukeleigh.adm' -p'1ukE1Eigh.3742'
+    
+}
