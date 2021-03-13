6a7c13d75f11e008fa1490fc2f3c33df37a30452
Luke Leigh <luke.leigh@gmail.com>
2021-03-13 14:02:23 +0000

    Create New-SYDIDocument.ps1

---
+++ New-SYDIDocument.ps1
@@ -0,0 +1,14 @@
+function New-SYDIDocument {
+    param (
+        $ComputerName,
+        $Username,
+        $Password,
+        $OutputPath
+        )
+        
+        $SYDIPath = "$PSScriptRoot\sydi-server-2.4\Sydi-Server.vbs"
+        $Filename = $OutputPath + $ComputerName + ".docx"
+
+    cscript.exe $SYDIPath -wabefghipPqrsSu -racdklp -ew -f10 -d -o"$FileName" -t"$ComputerName" -u"$Username" -p"$Password"
+    
+}
