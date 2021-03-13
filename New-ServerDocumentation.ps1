35b0c2f9429517dafd153c8d694c2b3e7423317b
Luke Leigh <luke.leigh@gmail.com>
2021-03-13 13:46:00 +0000

    Update New-ServerDocumentation.ps1

--- New-ServerDocumentation.ps1
+++ New-ServerDocumentation.ps1
@@ -1,8 +1,14 @@
 function New-ServerDocumentation {
     param (
-        OptionalParameters
-    )
-    
-    cscript.exe D:\LazyWinAdmin\scripts\Sydi-Server.vbs -wabefghipPqrsSu -racdklp -ew -f10 -d -oD:\ServerDocumentation\ACS02.docx -tACS02 -u'lukeleigh.adm' -p'1ukE1Eigh.3742'
+        $ComputerName,
+        $Username,
+        $Password,
+        $OutputPath
+        )
+        
+        $SYDIPath = "$PSScriptRoot\sydi-server-2.4\Sydi-Server.vbs"
+        $Filename = $OutputPath + $ComputerName + ".docx"
+
+    cscript.exe $SYDIPath -wabefghipPqrsSu -racdklp -ew -f10 -d -o"$FileName" -t"$ComputerName" -u"$Username" -p"$Password"
     
 }
